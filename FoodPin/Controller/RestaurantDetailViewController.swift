8af8eb93261a591ceb30f7a9f5ba34bafe65a15c
Dongyu Zhang <>
2018-09-29 12:42:43 -0400

    Add Rating Function

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -14,6 +14,28 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     @IBOutlet var tableView: UITableView!
     @IBOutlet var headerView: RestaurantDetailHeaderView!
     
+    @IBAction func close(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func rateRestaurant(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: {
+            if let rating = segue.identifier {
+                self.restaurant.rating = rating
+                self.headerView.ratingImageView.image = UIImage(named: rating)
+                
+                let scaleTransform = CGAffineTransform.init(scaleX: 0.1, y: 0.1)
+                self.headerView.ratingImageView.transform = scaleTransform
+                self.headerView.ratingImageView.alpha = 0
+                
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.7, options: [], animations: {
+                    self.headerView.ratingImageView.transform = .identity
+                    self.headerView.ratingImageView.alpha = 1
+                }, completion: nil)
+            }
+        })
+    }
+    
     var restaurant: Restaurant = Restaurant()
     
     // MARK: - View controller life style
@@ -114,8 +136,13 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         if segue.identifier == "showMap" {
             let destinationController = segue.destination as! MapViewController
             destinationController.restaurant = restaurant
+            
+        } else if segue.identifier == "showReview" {
+            let destinationController = segue.destination as! ReviewViewController
+            destinationController.restaurant = restaurant
         }
     }
+    
 
 
 }
---
+++ FoodPin/Controller/ReviewViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by Dongyu Zhang on 9/28/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+    @IBOutlet var rateButtons: [UIButton]!
+    
+    @IBOutlet var closeButton: UIButton!
+    
+    var restaurant = Restaurant()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        backgroundImageView.image = UIImage(named: restaurant.image)
+        
+        // Applying the blur effect
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0)
+        let scaleUpTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
+        let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+
+        // Make the button invisible and move off the screen
+        for rateButton in rateButtons {
+            rateButton.transform = moveScaleTransform
+            rateButton.alpha = 0
+        }
+        
+        let moveUpTransform = CGAffineTransform.init(translationX: 0, y: -800)
+        closeButton.transform = moveUpTransform
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+            for index in 0...self.rateButtons.count - 1{
+                UIView.animate(withDuration: 0.4, delay: 0.1 + 0.05 * Double(index), options: [], animations: {
+                    self.rateButtons[index].transform = .identity
+                    self.rateButtons[index].alpha = 1.0
+                }, completion: nil)
+            }
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: [], animations: {
+            self.closeButton.transform = .identity
+        }, completion: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
